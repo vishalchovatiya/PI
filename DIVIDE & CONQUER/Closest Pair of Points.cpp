987acd11d931ffafe00ef20f44be858cc1ef5bf8
VisheshPatel <visheshchovatiya@gmail.com>
2016-03-02 16:34:50 +0530

    Closest Pair of Points.cpp

--- DIVIDE & CONQUER/Closest Pair of Points.cpp
+++ DIVIDE & CONQUER/Closest Pair of Points.cpp
@@ -9,8 +9,24 @@ using namespace std;
 
 
 /*
+
+	Following is a recap of the algorithm discussed in the previous post.
+
+		1) We sort all points according to x coordinates.
+		
+		2) Divide all points in two halves.
+		
+		3) Recursively find the smallest distances in both subarrays.
+		
+		4) Take the minimum of two smallest distances. Let the minimum be d.
+		
+		5) Create an array strip[] that stores all points which are at most d distance away from the middle line dividing the two sets.
+		
+		6) Find the smallest distance in strip[].
+		
+		7) Return the minimum of d and the smallest distance calculated in above step 6.
+
 	Time Complexity:-
-	
 
 	- Let Time complexity of above algorithm be T(n). 
 	- Let us assume that we use a O(nLogn) sorting algorithm. 
