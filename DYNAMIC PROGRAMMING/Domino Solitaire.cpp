d854bf271042c5b7ff7eeffbbfcd63690465a35c
VisheshPatel <visheshchovatiya@gmail.com>
2016-03-29 18:17:59 +0530

    Domino Solitaire.cpp

--- DYNAMIC PROGRAMMING/Domino Solitaire.cpp
+++ DYNAMIC PROGRAMMING/Domino Solitaire.cpp
@@ -53,34 +53,104 @@ using namespace std;
 	=> Sample Output:
 	
 	12
-
+	
+	Answer:-
+	
+	1). Single Column
+	
+	----
+	|8 |
+	----		Answer:- 8 - 9 = 1
+	|9 |
+	----
+		
+	2). Add one more Column	
+	
+	-------
+	|8 |6 |
+	-------
+	|9 |7 |
+	-------
+	
+	Tiling 1					Tiling 2
+	-------						----  ----
+	|8 |6 |						|8 |  |6 |
+	-------						----  ----		
+	-------						|9 |  |7 |
+	|9 |7 |						----  ----
+	-------
+	(8-6) + (9-7) = 2 + 2 = 4    (8-9) + (6-7) = 1 + 1 = 2					Answer here is 4
+	
+	3). Add one more Column	
+	
+	----------
+	|8 |6 |2 |
+	----------
+	|9 |7 |1 |
+	----------
+	Tilint 1
+	----  ----  ----
+	|8 |  |6 |  |2 |
+	----  ----  ----
+	|9 |  |7 |  |1 |
+	----  ----  ----
+	
+	(8-9) + (6-7) + (2-1) = 3
+	
+	Tilint 2
+	----  --------
+	|8 |  |6 |2 |
+	----  --------
+	|9 |  |7 |1 |
+	----  --------
+	
+	(8-9) + (6-2) + (7-1) = 11
+	
+	Tilint 3
+	-------  -----
+	|8 |6 |  |2 |
+	-------  -----
+	|9 |7 |  |1 |
+	-------  -----
+	
+	(8-6) + (9-7) + (2-1) = 5				Answer here is 11
+	
+	
+	Conclusion:- In above three steps, we can see that many steps are computer repeatedly, So there is overlapping sub problem
+	
 */
-
 
 int main() 
 {
-	int n, A[100001][2], B[100001], i, j;
-	
-    cin>>n;
+	int n, A[2][100001], B[100001], i, j;
+
+    cin>>n;    
     for(i=0;i<2;i++)
     {
         for(j=0;j<n;j++)
-            cin>>A[j][i];
+            cin>>A[i][j];
     }
 
-    B[0] = abs(A[0][0] - A[0][1]);
+    B[0] = abs(A[0][0] - A[1][0]);
     
-    B[1] = max( B[0] + abs(A[1][0]-A[1][1]) , abs(A[1][0]-A[0][0])+abs(A[1][1]-A[0][1]) );
+    //	8 6 2 3
+	// 	9 7 1 2
+    
+    B[1] = max( B[0] + abs( A[0][1] - A[1][1] ), abs( A[0][0] - A[0][1] ) + abs( A[1][0] - A[1][1] )  );
     
     for(i=2;i<n;i++)
     {
-    	B[i] = max( B[i-1] + abs(A[i][0]-A[i][1]) , B[i-2] + abs(A[i][0]-A[i-1][0]) + abs(A[i][1]-A[i-1][1]) );
-    }
-        
+    	B[i] = max( B[i-1] + abs( A[0][i] - A[1][i] ),  
+														B[i-2] + abs( A[0][i-1] - A[0][i] ) + abs( A[1][i-1] - A[1][i] )  );
+    }    
 
     cout<<B[n-1]<<endl;
-	
+/*    
+    for(i=0;i<n;i++)
+    {
+    	cout<<"B["<<i<<"] = "<<B[i]<<endl;
+    }
+*/	
 	return 0;
 }
-
 
