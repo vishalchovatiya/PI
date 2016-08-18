fbf876b820e6e78a5a010ec3b0e4c25020072fe2
Vishal Chovatiya <visheshchovatiya@gmail.com>
2016-08-18 10:24:30 +0530

    Update Print all distinct permutations of a given string- Iterative.cpp

--- ARRAY/Print all distinct permutations of a given string- Iterative.cpp
+++ ARRAY/Print all distinct permutations of a given string- Iterative.cpp
@@ -27,14 +27,14 @@ using namespace std;
 		
 	Algorithm:- (Source:https://en.wikipedia.org/wiki/Permutation#Generation_in_lexicographic_order)
     1. Find the largest index k such that a[k] < a[k + 1]. If no such index exists, the permutation is the last permutation.
-    2. Find the largest index l greater than k such that a[k] < a[l].
+    2. Find the index l such that k < l & a[k] < a[l].
     3. Swap the value of a[k] with that of a[l].
     4. Reverse the sequence from a[k + 1] up to and including the final element a[n].
     
     For example, given the sequence [1, 2, 3, 4], the algo steps are as follows:
 
     1. Index k = 2, because 3(value at index 2) satisfies condition of being the largest index that is still less than a[k + 1] which is 4.
-    2. Index l = 3, because 4 is the only value in the sequence that is greater than 3 in order to satisfy the condition a[k] < a[l].
+    2. Index l = 3, because 4(value at index 3) satisfy the condition a[k] < a[l] & k < l.
     3. The values of a[2] and a[3] are swapped to form the new sequence [1,2,4,3].
     4. The sequence after k-index a[2] to the final element is reversed. Because only one value lies after this index (the 3), the sequence remains unchanged in this instance. Thus the lexicographic successor of the initial state is permuted: [1,2,4,3].
 
