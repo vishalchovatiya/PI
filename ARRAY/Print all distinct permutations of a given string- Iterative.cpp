7a56a1d7fdce8ee3c7c876312bad21758eaa53d0
Vishal Chovatiya <visheshchovatiya@gmail.com>
2016-08-18 11:01:58 +0530

    Update Print all distinct permutations of a given string- Iterative.cpp

--- ARRAY/Print all distinct permutations of a given string- Iterative.cpp
+++ ARRAY/Print all distinct permutations of a given string- Iterative.cpp
@@ -27,7 +27,7 @@ using namespace std;
 		
 	Algorithm:- (Source:https://en.wikipedia.org/wiki/Permutation#Generation_in_lexicographic_order)
     1. Find the largest index k such that a[k] < a[k + 1]. If no such index exists, the permutation is the last permutation.
-    2. Find the index l such that k < l & a[k] < a[l].
+    2. Find the largest index l such that k < l & a[k] < a[l].
     3. Swap the value of a[k] with that of a[l].
     4. Reverse the sequence from a[k + 1] up to and including the final element a[n].
     
@@ -78,7 +78,7 @@ void sortedPermutations(char str[])
 		if( k == -1 )		
 			isFinish = true;
 		else
-		{	// Step 2: Find the largest index l greater than k such that a[k] < a[l]
+		{	// Step 2: Find the largest index l such that k < l & a[k] < a[l].
 			int l = findL( str, k, Size);
 
 			// Step 3: Swap the value of a[k] with that of a[l]
