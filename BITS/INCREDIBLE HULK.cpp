67dbf64ac8c62470998c988b1a673144a38a43d1
Vishal Chovatiya <visheshchovatiya@gmail.com>
2019-07-05 11:20:56 +0530

    Create INCREDIBLE HULK.cpp

---
+++ BITS/INCREDIBLE HULK.cpp
@@ -0,0 +1,19 @@
+/*
+Link: https://hack.codingblocks.com/contests/c/720/135
+Q: The Planet Earth is under a threat from the aliens of the outer space and the Marvel Avengers team is busy fighting against them. Meanwhile, The Incredible Hulk has to defeat an enemy who is N steps above the level where he is standing (assume it as the 0th level). Hulk, because of his incredible jumping ability can take jumps in power of 2. In order to defeat the enemy as quickly as possible he has to reach the Nth step in minimum moves possible. Help Hulk to find the same and contribute in saving the Mother Earth.
+
+Sample Input:
+3
+3
+4
+5
+
+Sample Output:
+2
+1
+2
+
+Explanation:
+Let total steps is n, find the nearest integer which is of power 2 and less then n. let it would be k. now remaining steps to cover is n-k and result = 1 + min steps for (n-k) remaining steps.
+
+*/
