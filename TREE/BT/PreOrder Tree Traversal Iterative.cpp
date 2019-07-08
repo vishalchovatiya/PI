86df1ee4d4e5839f212e61aa378a66cbf477864a
Vishal Chovatiya <visheshchovatiya@gmail.com>
2019-07-08 10:07:48 +0530

    Update PreOrder Tree Traversal Iterative.cpp

--- TREE/BT/PreOrder Tree Traversal Iterative.cpp
+++ TREE/BT/PreOrder Tree Traversal Iterative.cpp
@@ -1,102 +1,72 @@
-#include <iostream>
-#include <string.h>
-#include <list>
-#include <map>
-#include <stack>
-#include <queue>
+#include <bits/stdc++.h>
+
+#define DEBUG(X) std::cout << #X << " = " << X << std::endl;
+#define PRINT(C, WAY)                      \
+    do                                     \
+    {                                      \
+        cout << setw(10) << #C << " : \n"; \
+        for (auto &&i : C)                 \
+        {                                  \
+            WAY;                           \
+        }                                  \
+        cout << endl;                      \
+    } while (0);
+#define ALL(C) (C).begin(), (C).end()
+#define RALL(C) (C).rbegin(), (C).rend()
+#define PRESENT(C, X) ((C).find() != (C).end())
+
+using ll = long long;
 using namespace std;
 
-
-/*
-	PreOrder:-   First Parent, Then Child
-
-	Algo:-	
-		- Do until stack empty
-		- Fetch stack top and print;
-		- Push right child first, then left
-	
-		
-*/
-
-class Tree
+struct node
 {
-	class Node
-	{
-		public:
-			int data;
-			Node *left;	
-			Node *right;	
-	};
-	
-	Node *root = NULL;
-	
-	/*-------------------------------Utility Functions----------------------------------*/
-	Node *getNewNode(int data)
-	{
-		Node *temp = new Node;
-		temp->data = data;
-		temp->left = NULL;
-		temp->right = NULL;
-		
-		return temp;
-	}	
+    int data;
+    node *left;
+    node *right;
 
-	/*-----------------------------------------------------------------------------------*/
-
-	public:	
-		void PreOrder(Node *Root)
-		{
-			if( Root == NULL)	return;
-			
-			cout<<" "<<Root->data;					
-			PreOrder(Root->left);				
-			PreOrder(Root->right);			
-		}
-		
-		void PreOrderIterative(Node *Root)
-		{
-			stack<Node*>	S;
-			
-			S.push(Root);
-			
-			while( !S.empty())
-			{	
-				Node* temp = S.top();	
-				S.pop();
-				cout<<" "<<temp->data;		
-				
-				if( temp->right)	S.push(temp->right);
-				if( temp->left)		S.push(temp->left);
-			}
-		}
-
-
-		
-		void test()
-		{			
-			root = getNewNode(1);
-			root->left = getNewNode(2);
-			root->right = getNewNode(3);
-			root->left->left = getNewNode(4);
-			root->left->right = getNewNode(5);
-			root->left->right->right = getNewNode(6);
-
-			PreOrder(root);
-			cout<<endl;
-			PreOrderIterative( root);
-			cout<<endl;
-		}
+    node(int x = 0) : data(x), left(NULL), right(NULL) {}
 };
 
+void traversalIterative(node *root)
+{
+    unordered_map<node *, int> cnt;
+    stack<node *> S;
 
+    S.push(root);
 
+    while (!S.empty())
+    {
+        node *curr = S.top();
 
+        if (curr == NULL)
+        {
+            S.pop();
+            continue;
+        }
+
+        if (cnt[curr] == 0)cout << curr->data << " ";            
+        else if (cnt[curr] == 1)S.push(curr->left);            
+        else if (cnt[curr] == 2)S.push(curr->right);
+        else S.pop();
+        cnt[curr]++;
+    }
+    cout << endl;
+}
 
 int main()
 {
-	Tree T;
-	
-	T.test();	
-	
-	return 0;
+    node *root = new node(10);
+
+    root->left = new node(5);
+    root->right = new node(12);
+
+    root->left->left = new node(3);
+    root->left->right = new node(7);
+
+    root->right->left = new node(11);
+    root->right->right = new node(17);
+
+    traversalIterative(root);
+
+    return 0;
 }
