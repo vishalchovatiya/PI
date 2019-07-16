dc92fb80aaab38749c440df1bad456cc9dff45c2
Vishal Chovatiya <visheshchovatiya@gmail.com>
2019-07-16 21:33:15 +0530

    Update extended euclidean algo.cpp

--- MATH/extended euclidean algo.cpp
+++ MATH/extended euclidean algo.cpp
@@ -19,40 +19,6 @@ using ll = long long;
 using ull = unsigned long long;
 using namespace std;
 
-template <typename T>
-inline T is_odd(T n)
-{
-    return (n & 0x1);
-}
-
-/*
-    Explanation example 1: 2^3
-    3 = 0011
-          42 = 8
-    multiply base with set bit
-
-    Explanation example 2: 3^3
-    3 = 0011
-          93 = 27
-    where i is bit number
- */
-template <typename T>
-T binary_exponentiation(T x, T n, T m = numeric_limits<T>::max()) // x^n in log(N) time
-{
-    T res = 1;
-    while (n > 0)
-    {
-        if (n & 0x1)
-        {
-            res = (res * x) % m;
-        }
-        x = (x * x) % m;
-        n = n / 2;
-    }
-
-    return res;
-}
-
 ll extended_euclid(ll a, ll b, ll &x, ll &y)
 {
     if (b == 0)
