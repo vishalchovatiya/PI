dac3144bf77bc5ea997e39259ec968c6b7b4ce22
Vishal Chovatiya <visheshchovatiya@gmail.com>
2019-07-22 21:45:12 +0530

    Update Calculate value of nCr(combination), Binomial Coefficients.cpp

--- MATH/Calculate value of nCr(combination), Binomial Coefficients.cpp
+++ MATH/Calculate value of nCr(combination), Binomial Coefficients.cpp
@@ -1,26 +1,62 @@
-/*
-nCr = n! / (r! . (n-r)!)
-*/
+#include <iostream>
+#include <sstream>
+#include <cstdio>
+#include <cmath>
+#include <cstring>
+#include <cctype>
+#include <string>
+#include <chrono>
+#include <iomanip>
+#include <vector>
+#include <list>
+#include <set>
+#include <unordered_set>
+#include <map>
+#include <unordered_map>
+#include <queue>
+#include <numeric>
+#include <stack>
+#include <algorithm>
+#include <functional>
+using namespace std;
 
-#include <bits/stdc++.h>
+/* ------------------------- HELPERS DEFINE -------------------------- */
+template <typename T>
+ostream &operator<<(ostream &o, pair<const T, T> &pair) // To print map in PRINT
+{
+    o << pair.first << " " << pair.second << endl;
+    return o;
+}
 
 #define DEBUG(X) cout << #X << " = " << X << endl;
-#define PRINT(C, WAY)                      \
+#define PRINT(C)                           \
     do                                     \
     {                                      \
         cout << setw(10) << #C << " : \n"; \
         for (auto &&i : C)                 \
         {                                  \
-            WAY;                           \
+            cout << i << " ";              \
         }                                  \
         cout << endl;                      \
     } while (0);
 #define ALL(C) (C).begin(), (C).end()
-#define RALL(C) (C).rbegin(), (C).rend()
 #define PRESENT(C, X) ((C).find(X) != (C).end())
+#define BENCHMARK(f)         \
+    do                       \
+    {                        \
+        cout << #f << " = "; \
+        benchmark(f);        \
+    } while (0);
 
-using ll = long long;
-using namespace std;
+const auto benchmark = [](function<void(void)> f) {
+    chrono::steady_clock::time_point start = chrono::steady_clock::now();
+    f();
+    cout << chrono::duration_cast<chrono::microseconds>(chrono::steady_clock::now() - start).count() << " microseconds\n";
+};
+
+using ll = unsigned long long;
+
+/* -------------------------  MAIN CODE  -------------------------- */
 
 template <typename T>
 T factorial(T n, T max_upto = 10000, T mod = numeric_limits<T>::max())
@@ -42,13 +78,49 @@ T factorial(T n, T max_upto = 10000, T mod = numeric_limits<T>::max())
     return factorial[n];
 }
 
-ll nCr(ll n, ll r)
+ll nCr_slow(ll n, ll r)
 {
     return factorial(n) / (factorial(r) * factorial(n - r));
+}
+
+/* Pascal's Triangle line wise
+0|    1
+1|    1 1
+2|    1 2 1
+3|    1 3 3 1
+4|    1 4 6 4 1
+5|    1 5 10 10 5 1
+
+Note: each cell in pascal triangle is nCr, where n = line no, r = cell no in that line
+
+Formula:
+
+C(line, i)   = line! / ( (line-i)! * i! )
+C(line, i-1) = line! / ( (line - i + 1)! * (i-1)! )
+We can derive following expression from above two expressions.
+Just substitute value of line! from expression 2 to expression 1
+
+C(line, i) = C(line, i-1) * (line - i + 1) / i
+
+So C(line, i) can be calculated from C(line, i-1) in O(1) time
+ */
+
+template <typename T>
+T nCr(T n, T r) //time complexity O(N^2), space O(1)
+{
+    T &line = n;
+
+    T C = 1;
+    for (T i = 1; i <= r; i++)
+    {
+        C = C * (line - i + 1) / i;
+    }
+    return C;
 }
 
 int main()
 {
     DEBUG(nCr(3, 2));
+    DEBUG(nCr_slow(3, 2));
     return 0;
 }
