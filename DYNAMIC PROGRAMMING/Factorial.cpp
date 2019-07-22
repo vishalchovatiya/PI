5e06b02e444d917d40488abb9a4ceb808ee86cb2
Vishal Chovatiya <visheshchovatiya@gmail.com>
2019-07-22 12:22:05 +0530

    Update Factorial.cpp

--- DYNAMIC PROGRAMMING/Factorial.cpp
+++ DYNAMIC PROGRAMMING/Factorial.cpp
@@ -1,33 +1,46 @@
-#include <iostream>
-#include <string.h>
-#include <stdlib.h>
-#include <list>
-#include <map>
-#include <stack>
-#include <queue>
-using namespace std;
-
-
 /*
 		Factorial formula N! = N x (N-1)!
 */
 
+#include <bits/stdc++.h>
+
+#define DEBUG(X) cout << #X << " = " << X << endl;
+#define PRINT(C, WAY)                      \
+    do                                     \
+    {                                      \
+        cout << setw(10) << #C << " : \n"; \
+        for (auto &&i : C)                 \
+        {                                  \
+            WAY;                           \
+        }                                  \
+        cout << endl;                      \
+    } while (0);
+#define ALL(C) (C).begin(), (C).end()
+#define RALL(C) (C).rbegin(), (C).rend()
+#define PRESENT(C, X) ((C).find(X) != (C).end())
+
+using ll = long long;
+using namespace std;
+
+template <typename T>
+T get_factorial_of(T n, T max_upto = 10000)
+{
+    bool is_precomp = false;
+    T *factorial = new T[max_upto];
+    is_precomp = false;
+
+    factorial[0] = factorial[1] = 1;
+
+    for (T i = 2; i <= n; i++)
+    {
+        factorial[i] = i * factorial[i - 1];
+    }
+
+    return factorial[n];
+}
 
 int main()
 {
-	int N = 4;
-	int Fact[N];
-	int FactorialStorage[N];
-	
-	Fact[0] = 1;
-	Fact[1] = 1;
-	
-	for(int n=2; n<=N; n++)
-	{
-		Fact[n] = n * Fact[n-1];
-	}
-	
-	cout<<Fact[N]<<endl;
-	
-	return 0;
+    DEBUG(get_factorial_of(3));
+    return 0;
 }
