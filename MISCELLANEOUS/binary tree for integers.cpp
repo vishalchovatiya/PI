5d238f97c9c84354fa0997681d1d4bc746aff77c
Vishal Chovatiya <visheshchovatiya@gmail.com>
2019-07-24 12:00:56 +0530

    Update binary tree for integers.cpp

--- MISCELLANEOUS/binary tree for integers.cpp
+++ MISCELLANEOUS/binary tree for integers.cpp
@@ -22,10 +22,22 @@ using namespace std;
 
 /* ------------------------- HELPERS DEFINE -------------------------- */
 template <typename T>
-ostream &operator<<(ostream &o, pair<const T, T> &pair) // To print map in PRINT
+ostream &operator<<(ostream &o, pair<T, T> &pair) // To print map in PRINT
 {
     o << pair.first << " " << pair.second << endl;
     return o;
+}
+
+template <typename... T>
+void read(T &... args)
+{
+    ((cin >> args), ...);
+}
+
+template <typename... T>
+void write(T... args)
+{
+    ((cout << args), ...);
 }
 
 #define DEBUG(X) cout << #X << " = " << X << endl;
@@ -39,6 +51,7 @@ ostream &operator<<(ostream &o, pair<const T, T> &pair) // To print map in PRINT
         }                                  \
         cout << endl;                      \
     } while (0);
+
 #define ALL(C) (C).begin(), (C).end()
 #define PRESENT(C, X) ((C).find(X) != (C).end())
 #define BENCHMARK(f)         \
@@ -55,14 +68,21 @@ const auto benchmark = [](function<void(void)> f) {
 };
 
 using ll = long long;
-
-ll get_no_of_query();
-/* -------------------------  MAIN CODE  -------------------------- */
-
+using namespace std;
 struct bt
 {
     /*
         - data not needed, coz leaf node represent value
+        - edge represent binary value
+        - store binary of integers in reverse matter i.e. 0,1,5, 0xF will be store as
+
+                        0/  \1
+                       0/  0/\1
+                      0/   1\ \1
+                     0/\1  0/  \1
+
+        integers     0  1  5   0xF
+
     */
     struct node
     {
@@ -71,41 +91,65 @@ struct bt
     };
 
     node *root = NULL;
+    uint8_t bits = 4; // depth of tree
 
     bt() : root(new node) {}
 
     void add(ll value)
     {
-        ll idx = 3;
+        ll idx = (bits - 1);
         node *traverse = root;
 
-        while (idx >= 0) // 0...1
+        while (idx >= 0)
         {
             if (value & (1 << idx))
             {
                 if (traverse->right == NULL)
                     traverse->right = new node;
                 traverse = traverse->right;
-                DEBUG("RIGHT");
             }
             else
             {
                 if (traverse->left == NULL)
                     traverse->left = new node;
                 traverse = traverse->left;
-                DEBUG("LEFT");
             }
 
             idx--;
         }
     }
 
-    void preorder_traversal(node *root, int value = 0, int cnt = 0)
+    bool is_present(ll value)
+    {
+        ll idx = (bits - 1);
+        node *traverse = root;
+
+        while (idx >= 0)
+        {
+            if (value & (1 << idx))
+            {
+                if (traverse->right == NULL)
+                    return false;
+                traverse = traverse->right;
+            }
+            else
+            {
+                if (traverse->left == NULL)
+                    return false;
+                traverse = traverse->left;
+            }
+
+            idx--;
+        }
+        return true;
+    }
+
+    void preorder_traversal(node *root, ll value = 0, ll cnt = 0)
     {
         if (root->left == NULL && root->right == NULL)
         {
-            if (cnt == (4))
-                cout << cnt << " " << hex << value << endl;
+            if (cnt == (bits))
+                cout << value << endl;
             return;
         }
 
@@ -113,7 +157,7 @@ struct bt
             preorder_traversal(root->left, value, (cnt + 1));
 
         if (root->right != NULL)
-            preorder_traversal(root->right, value | (1 << cnt), (cnt + 1));
+            preorder_traversal(root->right, value | (1 << ((bits - 1) - cnt)), (cnt + 1));
     }
 
     void print()
@@ -128,7 +172,10 @@ int main()
     tree.add(0);
     tree.add(1);
     tree.add(2);
-    // tree.add(3);
+    tree.add(5);
+    DEBUG(tree.is_present(2));
+    DEBUG(tree.is_present(4));
+    DEBUG(tree.is_present(5));
     tree.print();
     return 0;
 }
