1a298e7695cc33dc427665aa40dcbfd14173f519
Vishal Chovatiya <visheshchovatiya@gmail.com>
2019-08-04 17:00:22 +0530

    Create Disjoint-Set, Union-Find data structure.cpp

---
+++ MISCELLANEOUS/Disjoint-Set, Union-Find data structure.cpp
@@ -0,0 +1,190 @@
+
+#include <iostream>
+#include <sstream>
+#include <cstdio>
+#include <cmath>
+#include <cstring>
+#include <cctype>
+#include <string>
+#include <chrono>
+#include <iomanip>
+#include <vector>
+#include <list>
+#include <set>
+#include <unordered_set>
+#include <map>
+#include <unordered_map>
+#include <queue>
+#include <numeric>
+#include <stack>
+#include <algorithm>
+#include <functional>
+using namespace std;
+
+/* ------------------------- HELPERS DEFINE -------------------------- */
+template <typename T>
+ostream &operator<<(ostream &o, pair<const T, T> &pair) // To print map in PRINT
+{
+    o << pair.first << " -> " << pair.second << endl;
+    return o;
+}
+
+template <typename... T>
+void read(T &... args)
+{
+    ((cin >> args), ...);
+}
+
+template <typename... T>
+void write(T... args)
+{
+    ((cout << args), ...);
+}
+
+#define DEBUG(X) cout << #X << " = " << X << endl;
+#define PRINT(C)                           \
+    do                                     \
+    {                                      \
+        cout << setw(10) << #C << " : \n"; \
+        for (auto &&i : C)                 \
+        {                                  \
+            cout << i << " ";              \
+        }                                  \
+        cout << endl;                      \
+    } while (0);
+
+#define ALL(C) (C).begin(), (C).end()
+#define PRESENT(C, X) ((C).find(X) != (C).end())
+#define BENCHMARK(f)         \
+    do                       \
+    {                        \
+        cout << #f << " = "; \
+        benchmark(f);        \
+    } while (0);
+
+const auto benchmark = [](function<void(void)> f) {
+    chrono::steady_clock::time_point start = chrono::steady_clock::now();
+    f();
+    cout << chrono::duration_cast<chrono::microseconds>(chrono::steady_clock::now() - start).count() << " microseconds\n";
+};
+
+using ll = long long;
+using vll = vector<ll>;
+using sll = set<ll>;
+using msll = multiset<ll>;
+using mll = map<ll, ll>;
+using mmll = multimap<ll, ll>;
+using namespace std;
+
+template <typename T = ll>
+class disjoint_set
+{
+    /* Typedefs ---------------------------------------------------------------------*/
+    using ll = long long;
+
+    /* Data Members ---------------------------------------------------------------------*/
+    struct node
+    {
+        T m_data;
+        ll m_rank = 0;
+        node *m_parent = nullptr;
+
+        node(T data) : m_data(data) {}
+    };
+
+    map<T, node *> m_dt_to_nd_ptr; // data to node pointers
+
+public:
+    /* Constructors ---------------------------------------------------------------------*/
+    disjoint_set() {}
+    ~disjoint_set() {}
+
+    void make_set(T data)
+    {
+        node *temp = new node(data);
+        temp->m_parent = temp;
+        m_dt_to_nd_ptr[data] = temp;
+    }
+
+    node *find_parent(node *nd)
+    {
+        node *traverse = nd;
+
+        while (nd != nd->m_parent) // Find parent
+        {
+            nd = nd->m_parent;
+        }
+
+        while (traverse != traverse->m_parent) // Set all node directly under parent
+        {
+            traverse->m_parent = nd;
+            traverse = traverse->m_parent;
+        }
+
+        return traverse;
+    }
+
+    T find_id(T data)
+    {
+        return find_parent(m_dt_to_nd_ptr[data])->m_data;
+    }
+
+    void make_union(T data_1, T data_2)
+    {
+        node *parent_1 = find_parent(m_dt_to_nd_ptr[data_1]);
+        node *parent_2 = find_parent(m_dt_to_nd_ptr[data_2]);
+
+        if (parent_1 == parent_2) // is of same union already
+            return;
+
+        if (parent_1->m_rank >= parent_2->m_rank)
+        {
+            parent_1->m_rank += (parent_1->m_rank == parent_2->m_rank) ? 1 : 0;
+            parent_2->m_parent = parent_1;
+        }
+        else
+        {
+            parent_1->m_parent = parent_2;
+        }
+    }
+
+    void print_sets() const
+    {
+        map<T, vector<T>> setid_to_vec;
+
+        for (auto &&nd : m_dt_to_nd_ptr)
+            setid_to_vec[nd.second->m_parent->m_data].push_back(nd.first);
+
+        for (auto &&id_vec : setid_to_vec)
+        {
+            cout << id_vec.first << " -> ";
+            for (auto &&val : id_vec.second)
+                cout << val << " ";
+            cout << endl;
+        }
+
+        // cout << "Total sets = " << setid_to_vec.size() << endl;
+    }
+};
+
+int main()
+{
+    disjoint_set<ll> ds;
+
+    ds.make_set(10);
+    ds.make_set(11);
+    ds.make_set(12);
+    ds.make_set(13);
+
+    // DEBUG(ds.find_id(10));
+    // DEBUG(ds.find_id(11));
+    // DEBUG(ds.find_id(12));
+    // DEBUG(ds.find_id(13));
+
+    ds.make_union(10, 11);
+    // ds.make_union(10, 12);
+
+    ds.print_sets();
+
+    return 0;
+}
