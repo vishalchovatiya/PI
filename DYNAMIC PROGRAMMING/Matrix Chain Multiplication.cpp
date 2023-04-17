a199e8d6f4b270c42a2518c1c168b005e57dca84
Vishal Chovatiya <visheshchovatiya@gmail.com>
2023-04-17 12:30:33 +0530

    Update Matrix Chain Multiplication.cpp

--- DYNAMIC PROGRAMMING/Matrix Chain Multiplication.cpp
+++ DYNAMIC PROGRAMMING/Matrix Chain Multiplication.cpp
@@ -1,100 +1,66 @@
 #include <iostream>
-#include <string.h>
-#include <stdlib.h>
-#include <climits>
-#include <list>
-#include <map>
-#include <stack>
-#include <queue>
+#include <algorithm>
+#include <vector>
+
 using namespace std;
 
+#define DEBUG(X) cout << #X << " = " << X << endl;
+#define ALL(X) begin(X), end(X)
 
-#define SIZE(A)		sizeof(A)/sizeof(A[0])
+
+
+uint64_t matrix_chain_multiplication_recursive(const vector<uint64_t>& p, uint64_t i, uint64_t j) {
+    if (i == j) return 0; 
+    uint64_t min_cost = numeric_limits<uint64_t>::max();
+    for (uint64_t k = i; k < j; ++k) {
+        uint64_t cost = matrix_chain_multiplication_recursive(p, i, k) 
+                        + matrix_chain_multiplication_recursive(p, k+1, j) 
+                        + p[i-1] * p[k] * p[j];
+        min_cost = min(min_cost, cost);
+    }
+    return min_cost;
+}
+
+// Matrix Chain Multiplication - Dynamic Programming - Top Down - Memoization
+uint64_t matrix_chain_multiplication_memoization(const vector<uint64_t>& p, uint64_t i, uint64_t j, vector<vector<uint64_t>>& memo) {
+    if (i == j) return 0; 
+    if (memo[i][j] != 0) return memo[i][j];
+    
+    uint64_t min_cost = numeric_limits<uint64_t>::max();
+    for (uint64_t k = i; k < j; ++k) {
+        uint64_t cost = matrix_chain_multiplication_memoization(p, i, k, memo) 
+                        + matrix_chain_multiplication_memoization(p, k+1, j, memo) 
+                        + p[i-1] * p[k] * p[j];
+        min_cost = min(min_cost, cost);
+    }
+    memo[i][j] = min_cost;
+    return min_cost;
+}
+
+
+int main() {
+    uint64_t n;
+    cin >> n;
+    vector<uint64_t>    matrix_chain_dimentions(n);
+    for (uint64_t i = 0; i < n; i++) {
+        cin >> matrix_chain_dimentions[i];
+    }
+
+    DEBUG(matrix_chain_multiplication_recursive(matrix_chain_dimentions, 1, n-1));
+
+    return 0;
+}
 
 /*
-	Question:- Given a sequence of matrices, find the most efficient way to multiply these matrices together 
-				OR Parenthesization of matrix multiplication to improve calculation speed
-	
-	- There are three Matrix say A, B, C with dimension of 1x2, 2x3, 3x4 respectively
-	
-	- if Multiply (AB)C = (1x2x3) x C = 6 + (1x3) x C = 6 + (1x3) x (3x4) = 6 + (1x3x4) = 6 + 12 =, gives 18
-	
-	- if Multiply A(BC) = A x (2x3x4) = A x 24(2x4) = 24 + (1x2) x (2x4) = 24 + (1x2x4) = 24 + 8 =, gives 32
-	
-	- By above different parenthesization, we conclude that (AB)C is best way to parenthesize
-	
-	- This problem has both DP Properties Optimal Sub-structure & Overlapping Sub-problem
-	
-	- M[i,j] = M[i,k] + M[k+1,j] + P[i-1]*P[k]*P[j]
-*/
+/CP/build/main < ../input.txt > ../output.txt
+
+Test Case 1:
+
+Test Case 2:
 
 
+Test Case 3:
 
+Test Case 4:
 
-/*----------------------Naive Method( Brute Force)------------------------------------------------------------------*/
-
-int MatrixChainOrder(int P[], int i, int j)
-{
-	if( i==j )	return 0;
-	
-	int Min = INT_MAX;
-	
-	for(int k=i; k<j; k++)
-	{
-		Min = min( Min, MatrixChainOrder( P, i, k) + MatrixChainOrder( P, k+1, j) + P[i-1] * P[k] * P[j] );
-	}
-	
-	return Min;
-}
-
-/*------------------------------------------------------------------------------------------------------------------*/
-
-
-
-
-
-/*------------------- Memoization OR Top-Down ---------------------------------------------------------------------*/
-
-int MatrixChainOrderDynamic(int P[], int i, int j, int **Table)
-{
-	if( i==j )	return 0;
-	
-	if( Table[i][j] == INT_MAX )
-	{	
-		for(int k=i; k<j; k++)
-		{
-			Table[i][j] = min( Table[i][j] , MatrixChainOrderDynamic( P, i, k, Table) + MatrixChainOrderDynamic( P, k+1, j, Table) + P[i-1] * P[k] * P[j] );
-		}
-	}
-	
-	return Table[i][j];
-}
-
-/*------------------------------------------------------------------------------------------------------------------*/
-
-
-
-
-
-
-int main()
-{
-	int A[] = { 1, 2, 3, 4};
-	
-	int **Table;
-	
-	Table = new int*[SIZE(A)];
-	
-	for(int Row=0; Row<SIZE(A); Row++)
-	{
-		Table[Row] = new int[SIZE(A)];
-		for(int Column=0; Column<SIZE(A); Column++)
-		{
-			Table[Row][Column] = INT_MAX;
-		}
-	}
-	
-	cout<<MatrixChainOrder( A, 1, SIZE(A)-1);
-	
-	return 0;
-}
+ */
